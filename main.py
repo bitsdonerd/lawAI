3da0a60948d4a6776c1be085c107cefc75d4ba3e
bitsdonerd <lukasousa70@gmail.com>
2025-02-12 17:15:12 -0300

    Test

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import os
 import openai
 from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+openai_key = os.getenv("OPENAI_API_KEY")
+
+def set_response(mensagens, openai_key, modelo="gpt-4o-mini",temperature=0.5, strem=False):
+    openai.api_key = openai_key
+    response = openai.Completion.create(
+        model=modelo,
+        messages=mensagens,
+        temperature=temperature,
+        strem=strem
+    )
+    return response
